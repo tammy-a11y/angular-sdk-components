ec1ecfe1613affdb50eba1da1a7c1db5cbd7bdf7
4manasa <manasamashetty96@gmail.com>
2025-09-09 19:42:38 +0530

    Hiding the EmbedListUUID column in embeddedData table and default labels not displaying in embedded data issue fixed (#323)
    * Hiding the EmbedListUUID column in embeddedData table
    
    * default labels not displaying in embedded data issue fixed
    
    * fix: label visibility issue
    
    ---------
    
    Co-authored-by: manasa <manasa.mashetty@in.pega.com>
    Co-authored-by: Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>

--- packages/angular-sdk-components/src/lib/_components/template/simple-table-manual/simple-table-manual.component.ts
+++ packages/angular-sdk-components/src/lib/_components/template/simple-table-manual/simple-table-manual.component.ts
@@ -306,6 +306,7 @@ export class SimpleTableManualComponent implements OnInit, OnDestroy {
     this.defaultView = editModeConfig ? editModeConfig.defaultView : viewForAddAndEditModal;
     this.bUseSeparateViewForEdit = editModeConfig ? editModeConfig.useSeparateViewForEdit : useSeparateViewForEdit;
     this.editView = editModeConfig ? editModeConfig.editView : viewForEditModal;
+    const primaryFieldsViewIndex = resolvedFields.findIndex(field => field.config.value === 'pyPrimaryFields');
     // const showDeleteButton = !this.readOnlyMode && !hideDeleteRow;
 
     // Nebula has other handling for isReadOnlyMode but has Cosmos-specific code
@@ -316,8 +317,11 @@ export class SimpleTableManualComponent implements OnInit, OnDestroy {
     //  Nebula does). It will also have the "label", and "meta" contains the original,
     //  unchanged config info. For now, much of the info here is carried over from
     //  Nebula and we may not end up using it all.
-    this.fieldDefs = buildFieldsForTable(rawFields, resolvedFields, showDeleteButton);
-
+    this.fieldDefs = buildFieldsForTable(rawFields, this.pConn$, showDeleteButton, {
+      primaryFieldsViewIndex,
+      fields: resolvedFields
+    });
+    this.fieldDefs = this.fieldDefs?.filter(field => !(field.meta?.config?.hide === true));
     this.initializeDefaultPageInstructions();
 
     // end of from Nebula
@@ -333,10 +337,15 @@ export class SimpleTableManualComponent implements OnInit, OnDestroy {
     //  from from the fieldDefs. This "name" is the value that
     //  we'll share to connect things together in the table.
 
+    const labelsMap = this.fieldDefs.reduce((acc, curr) => {
+      return { ...acc, [curr.name]: curr.label };
+    }, {});
+
     this.processedFields = [];
 
     this.processedFields = resolvedFields.map((field, i) => {
       field.config.name = this.displayedColumns[i]; // .config["value"].replace(/ ./g,"_");   // replace space dot with underscore
+      field.config.label = labelsMap[field.config.name] || field.config.label;
       return field;
     });
 
@@ -992,24 +1001,36 @@ export class SimpleTableManualComponent implements OnInit, OnDestroy {
     this.referenceList.forEach((element, index) => {
       const data: any = [];
       this.rawFields?.forEach(item => {
-        item = {
-          ...item,
-          config: { ...item.config, label: '', displayMode: this.readOnlyMode || this.allowEditingInModal ? 'DISPLAY_ONLY' : undefined }
-        };
-        const referenceListData = getReferenceList(this.pConn$);
-        const isDatapage = referenceListData.startsWith('D_');
-        const pageReferenceValue = isDatapage ? `${referenceListData}[${index}]` : `${this.pConn$.getPageReference()}${referenceListData}[${index}]`;
-        const config = {
-          meta: item,
-          options: {
-            context,
-            pageReference: pageReferenceValue,
-            referenceList: referenceListData,
-            hasForm: true
+        if (!item?.config?.hide) {
+          let label = '';
+          if (!this.readOnlyMode) {
+            label = item.config.readOnly ? '' : item.config.label;
           }
-        };
-        const view = PCore.createPConnect(config);
-        data.push(view);
+          item = {
+            ...item,
+            config: {
+              ...item.config,
+              label,
+              displayMode: this.readOnlyMode || this.allowEditingInModal ? 'DISPLAY_ONLY' : undefined
+            }
+          };
+          const referenceListData = getReferenceList(this.pConn$);
+          const isDatapage = referenceListData.startsWith('D_');
+          const pageReferenceValue = isDatapage
+            ? `${referenceListData}[${index}]`
+            : `${this.pConn$.getPageReference()}${referenceListData}[${index}]`;
+          const config = {
+            meta: item,
+            options: {
+              context,
+              pageReference: pageReferenceValue,
+              referenceList: referenceListData,
+              hasForm: true
+            }
+          };
+          const view = PCore.createPConnect(config);
+          data.push(view);
+        }
       });
       eleData.push(data);
     });
